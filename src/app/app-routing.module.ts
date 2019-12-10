f0342d15ddd5ed73a6d3a1d1b3106756f6e9a940
repaliwa <revanshu.paliwal@appdynamics.com>
2019-12-10 14:56:00 +0530

    basic routing for angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,51 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CourseListComponent } from './course-list/course-list.component';
+import { QuestionsListComponent } from './questions-list/questions-list.component';
 
+// const routes: Routes = [
+//   {
+//     path: '',
+//     component: CategoriesComponent,
+//     resolve: {
+//       data: CategoriesResolver
+//     }
+//   },
+//   {
+//     path: 'questions/about/:categorySlug',
+//     component: CategoryQuestionsComponent,
+//     resolve: {
+//       data: CategoryQuestionsResolver
+//     }
+//   },
+//   {
+//     path: 'question/:questionSlug',
+//     component: QuestionAnswersComponent,
+//     resolve: {
+//       data: QuestionAnswersResolver
+//     }
+//   }
+// ];
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CourseListComponent,
+    // resolve: {
+    //   data
+    // }
+  },
+  {
+    path: 'questions/:courseSelected',
+    component: QuestionsListComponent,
+    // resolve: {
+    //   data
+    // }
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: false})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { QuestionsListComponent } from './questions-list/questions-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNewComponentComponent,
+    CourseListComponent,
+    QuestionsListComponent
   ],
   imports: [
     BrowserModule,
